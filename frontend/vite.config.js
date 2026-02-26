271342f65c3b552458031e961ede721ab787a17a
rohanbanda-TRT <rohanb@thirdrocktechkno.com>
2026-02-26 19:11:06 +0530

    ot the true for allowed host

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,12 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     port: 3000,
-    allowedHosts: 'all',
+    allowedHosts: true,   // ✅ allow all hosts
     proxy: {
       '/api': {
         target: 'http://localhost:8000',
